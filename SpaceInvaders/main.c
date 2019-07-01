08fd66082092a0f06ed51214f8dce4cf3907c5e1
Gabriel Calo <gcalo@itba.edu.ar>
2019-07-01 15:12:03 -0300

    allegro example

--- SpaceInvaders/main.c
+++ SpaceInvaders/main.c
@@ -1,11 +1,37 @@
-
-#include <stdlib.h>
-
-
-
-int main(void) {
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
 
 
+int main(int argc, char** argv) {
 
-	return;
+	ALLEGRO_DISPLAY* display = NULL;
+
+	if (!al_init()) {
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
+	}
+
+	display = al_create_display(640, 480);
+	if (!display) {
+		fprintf(stderr, "failed to create display!\n");
+		return -1;
+	}
+
+	//al_init_primitives_addon();
+
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	al_set_target_backbuffer(display);
+
+	al_draw_triangle(10, 10, 20, 20, 30, 30, al_map_rgb(255, 255, 255), 0.5);
+
+	al_flip_display();
+
+	al_rest(10.0);
+
+	al_destroy_display(display);
+
+	return 0;
 }
